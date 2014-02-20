b2622d21ec6e3b8186e5d71bc53820990faf0140
jrfnl <github_nospam@adviesenzo.nl>
2014-02-20 03:27:51 +0100

    Add LI share button, update about page

--- page/footer.php
+++ page/footer.php
@@ -8,10 +8,13 @@ print '
 <div class="footer"><div>
 	<p id="share">
 		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://phpcheatsheets.com/" data-text="These PHP cheat sheets are awesome!" data-via="jrf_nl" data-related="jrf_nl" title="Share this website on Twitter">Tweet</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script>
+		<script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script>
+		
+		<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+		<script type="IN/Share" data-url="http://phpcheatsheets.com/" data-counter="right"></script>
 	</p>
 	<p id="license">This PHP cheat sheet collection is inspired by the <a href="http://www.blueshoes.org/en/developer/php_cheat_sheet/" target="_blank">Blueshoes cheat sheet</a> and licensed under the <a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GPLv3</a> software license. You can view and fork the source on <a href="http://github.com/jrfnl/PHP-cheat-sheet-extended" target="_blank">GitHub</a>. Pull requests welcome.</p>
-	<p id="copyright">&copy; 2006-' . date( 'Y' ) . ' <a href="http://adviesenzo.nl/">Advies en zo, Meedenken en -doen</a></p>
+	<p id="copyright">&copy; 2006-' . date( 'Y' ) . ' <a href="index.php?page=about">Advies en zo, Meedenken en -doen</a></p>
 
 </div></div>
 
