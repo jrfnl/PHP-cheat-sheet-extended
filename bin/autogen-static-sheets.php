a74a5fea3a834f2ef054a78b1181787feb9f528c
jrfnl <github_nospam@adviesenzo.nl>
2014-12-22 22:14:55 +0100

    Set up autogenerating of static sheets

---
+++ bin/autogen-static-sheets.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Notify user of what we're doing
+ */
+echo 'Generating static sheets for PHP ' . PHP_VERSION . PHP_EOL;
+
+
+/**
+ * Allow as much memory as possible by default
+ */
+if ( extension_loaded( 'suhosin' ) && is_numeric( ini_get( 'suhosin.memory_limit' ) ) ) {
+    $limit = ini_get( 'memory_limit' );
+    if ( preg_match( '(^(\d+)([BKMGT]))', $limit, $match ) ) {
+        $shift = array( 'B' => 0, 'K' => 10, 'M' => 20, 'G' => 30, 'T' => 40 );
+        $limit = ( $match[1] * ( 1 << $shift[$match[2]] ) );
+    }
+    if ( ini_get( 'suhosin.memory_limit' ) > $limit && $limit > -1 ) {
+        ini_set( 'memory_limit', ini_get( 'suhosin.memory_limit' ) );
+    }
+}
+else {
+    ini_set( 'memory_limit', -1 );
+}
+
+
+/**
+ * Set the environment
+ */
+// One-up as we're in /bin
+define( 'APP_DIR', dirname( dirname( __FILE__ ) ) );
+
+include_once( APP_DIR . '/include/setup-env.php' );
+
+// Overrule some variables
+$min     = '.min';
+$dir     = '../../';
+$autogen = true;
+
+define( 'SAVE_DIR', APP_DIR . '/static_results' );
+define( 'QUIZ_DIR', APP_DIR . '/quiz' );
+define( 'QUIZ', QUIZ_DIR . '/20131005-questions.php' );
+define( 'QUIZ_SAVE_DIR', QUIZ_DIR . '/static_results' );
+
+
+/**
+ * Generate the static sheets
+ */
+$types = array(
+	'compare'    => 'PHP Variable Comparison',
+	'arithmetic' => 'PHP Arithmetic Operations',
+	'test'       => 'PHP Variable Testing',
+);
+
+
+foreach( $types as $type => $page_title ) {
+
+	ob_start();
+
+	/**
+	 * Load a cheatsheet page
+	 */
+	$class = 'Vartype' . ucfirst( $type );
+	$file  = 'class.vartype-' . $type . '.php';
+
+	if ( isset( $type ) && file_exists( APP_DIR . '/' . $file ) ) {
+		include_once( APP_DIR . '/' . $file );
+		$current_tests = new $class();
+	
+		$tab = null;
+
+		include_once( APP_DIR . '/page/header.php' );
+		include_once( APP_DIR . '/page/notes-legend.php' );
+
+		$current_tests->do_page( true );
+
+		include_once( APP_DIR . '/page/footer.php');
+	}
+
+	$static_page = ob_get_clean();
+	$filename    = SAVE_DIR . '/php' . PHP_VERSION . '.html';
+
+	save_to_file( $filename, $static_page );
+}
+unset( $type, $page_title, $class, $file, $current_tests, $tab, $static_page, $filename );
+
+
+
+if ( is_file( QUIZ ) ) {
+	ob_start();
+	
+	include_once( APP_DIR . '/page/header.php' );
+	include_once( QUIZ );
+	include_once( APP_DIR . '/page/footer.php');
+
+	$static_page = ob_get_clean();
+	$filename    = QUIZ_SAVE_DIR . '/php' . PHP_VERSION . '.html';
+
+	save_to_file( $filename, $static_page );
+}
+
+
+function save_to_file( $filename, $contents ) {
+	if ( $contents !== false && is_string( $contents ) && $contents !== '' ) {
+		if ( file_put_contents( $filename, $contents ) !== false ) {
+			echo 'SUCCESS - created file : ' . $filename . PHP_EOL;
+		}
+		else {
+			echo 'FAILED to WRITE file : ' . $filename . PHP_EOL;
+		}
+	}
+	else {
+		echo 'FAILED to GENERATE file : ' . $filename . PHP_EOL;
+	}
+}
+
+
+
--- index.php
+++ index.php
@@ -1,46 +1,11 @@
 <?php
 
-if ( ! defined( 'E_STRICT' ) ) {
-	define( 'E_STRICT', 2048 );
-}
-error_reporting( E_ALL & ~E_STRICT );
-@ini_set( 'log_errors', false );
-
-
 define( 'APP_DIR', dirname( __FILE__ ) );
+include_once( APP_DIR . '/include/setup-env.php' );
 
-include_once( APP_DIR . '/include/xvardump.php' );
-include_once( APP_DIR . '/include/functions.php' );
-set_error_handler( 'do_handle_errors' );
-
-if ( ! defined( 'PHP_VERSION_ID' ) ) {
-	$version = PHP_VERSION;
-	define( 'PHP_VERSION_ID', (int) ( $version{0} * 10000 + $version{2} * 100 + $version{4} ) );
-}
-
-// Use C locale for Ctype functions
-setlocale( LC_CTYPE, 'C' );
-
-// Set timezone
-if ( PHP_VERSION_ID > 50100 ) {
-	date_default_timezone_set( 'UTC' );
-}
-
-
-// Minified js & css ?
-if ( ( isset( $_SERVER['HTTP_HOST'] ) && $_SERVER['HTTP_HOST'] === 'phpcheatsheets.com' ) || ( isset( $_SERVER['SERVER_NAME'] ) && $_SERVER['SERVER_NAME'] === 'phpcheatsheets.com' ) ) {
-	$min = '.min';
-}
-else {
-	$min = '';
-}
-
-
-// js & css directory change ?
-$dir = '';
-
-
-
+/**
+ * Determine what has been requested
+ */
 $type = null;
 $page = null;
 $page_title = 'PHP Cheat Sheets';
