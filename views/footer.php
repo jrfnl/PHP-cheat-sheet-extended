95ef0fb8caeac48ce42953187502d82467c03334
jrfnl <github_nospam@adviesenzo.nl>
2015-03-31 07:15:31 +0200

    Fix minor HTML error

--- views/footer.php
+++ views/footer.php
@@ -24,10 +24,10 @@ if ( ! defined( 'APP_DIR' ) ) {
 
 		<div id="paypal-donate">
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="8HS8MNK72DLH2">
-				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-				<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
+				<input type="hidden" name="cmd" value="_s-xclick" />
+				<input type="hidden" name="hosted_button_id" value="8HS8MNK72DLH2" />
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+				<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1" />
 			</form>
 		</div>
 
