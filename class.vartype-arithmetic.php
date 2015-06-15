d5407827eced04b9b301bb85927f45ada8a07a04
jrfnl <github_nospam@adviesenzo.nl>
2015-06-15 09:11:48 +0200

    Add link to BCMath gists as footnote to all BCMath functions

--- class.vartype-arithmetic.php
+++ class.vartype-arithmetic.php
@@ -156,6 +156,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => '$r = bcadd( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 		'bcsub'          => array(
@@ -165,6 +166,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => '$r = bcsub( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 		'bcmul'          => array(
@@ -174,6 +176,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => '$r = bcmul( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 		'bcdiv'          => array(
@@ -183,6 +186,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => '$r = bcdiv( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 		'bcmod'          => array(
@@ -192,6 +196,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => '$r = bcmod( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 
@@ -203,6 +208,7 @@ class VartypeArithmetic extends VartypeCompare {
 			'function'      => 'if ( $a != 0 && is_infinite( pow( $a, $b ) ) === false ) { $r = bcpow( $a, $b ); if ( is_string( $r ) ) { pr_str( $r ); } else { pr_var ( $r, \'\', true, true ); } } else { trigger_error( \'would result in INF and will normally exhaust memory\', E_USER_ERROR ); }',
 			'notes'         => array(
 				'<p>For this cheat sheet <code>bcscale()</code> has been set to 3. Remember that the default is 0.</p>',
+				'<p>For a reliable implementation of all the BCMath functions which avoids a number of the common pitfalls, see <a href="https://gist.github.com/jrfnl/8449978" target="_blank">this example function</a> (gist).</p>',
 			),
 		),
 		*/
