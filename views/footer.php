ea4447aa9e6afef08610dac38cc00e164e5a2a79
jrfnl <jrfnl@users.noreply.github.com>
2018-06-24 19:25:20 +0200

    Update social media & donate button code
    Verified and where necessary, updated, the code for social media and donate buttons.

--- views/footer.php
+++ views/footer.php
@@ -19,10 +19,10 @@ if ( ! defined( 'APP_DIR' ) ) {
 <div class="footer">
 	<div>
 		<p id="share">
-			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://phpcheatsheets.com/" data-text="These PHP cheat sheets are awesome!" data-via="jrf_nl" data-related="jrf_nl" title="Share this website on Twitter">Tweet</a>
-			<script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			<a href="https://twitter.com/share" class="twitter-share-button" data-text="These PHP cheat sheets are awesome!" data-url="https://phpcheatsheets.com/" data-via="jrf_nl" data-hashtags="php" data-related="jrf_nl,phpcheatsheets" title="Share this website on Twitter">Tweet</a>
+			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-			<script src="http://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+			<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
 			<script type="IN/Share" data-url="http://phpcheatsheets.com/" data-counter="right"></script>
 
 		</p>
@@ -32,12 +32,12 @@ if ( ! defined( 'APP_DIR' ) ) {
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_s-xclick" />
 				<input type="hidden" name="hosted_button_id" value="8HS8MNK72DLH2" />
-				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+				<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 				<img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1" />
 			</form>
 		</div>
 
-		<p id="call-to-donate">Like what you see ? Find this useful ? You can show your appreciation for the effort which has gone into creating these cheatsheets by giving a donation to the <a href="http://remembermewhenimgone.org/" title="Visit the Remember me when I'm gone project">Remember me when I&#39;m gone</a> non-profit project. Thanks!
+		<p id="call-to-donate">Like what you see ? Find this useful ? You can show your appreciation for the effort which has gone into creating these cheatsheets by giving a donation to the <a href="http://remembermewhenimgone.org/" title="Visit the Remember me when I'm gone project">Remember me when I&#39;m gone</a> non-profit project, a charity which is close to my heart. Thanks!
 		</p>
 
 		<p id="copyright">&copy; 2006-<?php echo date( 'Y' ); ?> <a href="<?php echo BASE_URI; ?>about/">Advies en zo, Meedenken en -doen</a></p>
